c1d9d732d15d2a7756b027d1d8fcc201d17689d1
faslijalal <jalalfasli853@gmail.com>
2026-02-23 11:49:25 +0800

    kotegori finish

--- kategori.php
+++ kategori.php
@@ -18,26 +18,23 @@
 </head>
 <body>
      <!-- Begin Page Content -->
-                <div class="container-fluid">
-
+<div class="container-fluid">
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Kategori</h1>
-                 
-
+    <h1 class="h3 mb-2 text-gray-800">Kategori</h1>
                     <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Tambah Kategori</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Nama Kategori</th>
-                                            <?php if($_SESSION['user']['level']=='admin'): ?>
-                                            <th>Aksi</th>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <a href="?kategori_tambah"></a>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama Kategori</th>
+                            <?php if($_SESSION['user']['level']=='admin'): ?>
+                            <th>Aksi</th>
                                             <?php endif;?>
                                         </tr>
                                     </thead>
@@ -61,7 +58,12 @@
                                         <tr>
                                             <td><?=  $i++; ?></td>
                                             <td><?=  $data['kategori'];?></td>
-                                            <td></td>
+                                            <td>
+                                                <i href="?page=kategori_ubah&&id=<?=$data['id_kategori'];?>" class="fa-solid fa-pen-to-square mr-5"></i>
+                                                <i href="?page=kategori_hapus&&id=<?=$data['id_kategori'];?>" class="fa-solid fa-trash " 
+                                                onclick="return confirm('Apakah Anda Yakin?')"></i>
+                                
+                                            </td>
                                         </tr>
                                         <?php endwhile;?>
                                     </tbody>
